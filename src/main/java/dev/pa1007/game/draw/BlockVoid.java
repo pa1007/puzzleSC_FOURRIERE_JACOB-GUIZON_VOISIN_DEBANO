debf11caa9e15d865cc5a27856aab43d55da2b32
Louis voisin <louis.voisin54@outlook.fr>
2020-09-17 10:26:44 +0200

    create blockVoid

---
+++ src/main/java/dev/pa1007/game/draw/BlockVoid.java
@@ -0,0 +1,9 @@
+package dev.pa1007.game.draw;
+
+import dev.pa1007.game.Block;
+import dev.pa1007.game.Position;
+
+public class BlockVoid extends Block {
+
+    public BlockVoid(Position currentPos) { super(0, currentPos); }
+}
