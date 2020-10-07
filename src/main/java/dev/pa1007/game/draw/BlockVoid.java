aeb4a767a4b09415cc302b8f5fa7cf689b8b1c7c
Paul-Alexandre Fourriere <pa.fourriere@gmail.com>
2020-10-07 21:32:26 +0200

    Add image in Block void if needed

--- src/main/java/dev/pa1007/game/draw/BlockVoid.java
+++ src/main/java/dev/pa1007/game/draw/BlockVoid.java
@@ -1,12 +1,28 @@
 package dev.pa1007.game.draw;
 
+import dev.pa1007.MainApp;
 import dev.pa1007.game.Block;
 import dev.pa1007.game.Position;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class BlockVoid extends Block {
+
+    private BufferedImage image;
 
     public BlockVoid(Position startPos) {
         super(-1, startPos);
         this.setCurrentPos(startPos);
+        try {
+            image = ImageIO.read(MainApp.class.getResource("images/void.png"));
+        }
+        catch (IOException ignored) {
+            image = new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB);
+        }
+    }
+
+    public BufferedImage getImage() {
+        return image;
     }
 }
