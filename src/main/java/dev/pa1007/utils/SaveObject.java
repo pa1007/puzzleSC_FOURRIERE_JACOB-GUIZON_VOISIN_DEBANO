5764cd4d32aac4b088b3fe6be4a86ba2ca20bc20
Paul-Alexandre Fourriere <pa.fourriere@gmail.com>
2020-11-19 11:10:37 +0100

    Create an object to save

---
+++ src/main/java/dev/pa1007/utils/SaveObject.java
@@ -0,0 +1,49 @@
+package dev.pa1007.utils;
+
+import dev.pa1007.game.Puzzle;
+import org.apache.commons.io.FileUtils;
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Base64;
+
+public class SaveObject implements Serializable {
+
+    private Puzzle puzzle;
+
+    private Boolean graph;
+
+    private String image;
+
+    public SaveObject(Puzzle puzzle, File pathImage) throws IOException {
+        this.puzzle = puzzle;
+        saveImage(pathImage);
+        graph = true;
+    }
+
+    public SaveObject(Puzzle puzzle) {
+        this.puzzle = puzzle;
+    }
+
+    public Puzzle getPuzzle() {
+        return puzzle;
+    }
+
+    public Boolean getGraph() {
+        return graph;
+    }
+
+    public Image getImage() throws IOException {
+        byte[] decodedBytes = Base64.getDecoder().decode(image);
+        File   file         = new File("savedImage.jpg");
+        FileUtils.writeByteArrayToFile(file, decodedBytes);
+        return ImageIO.read(file);
+    }
+
+    private void saveImage(File path) throws IOException {
+        byte[] fileContent = FileUtils.readFileToByteArray(path);
+        image = Base64.getEncoder().encodeToString(fileContent);
+    }
+}
